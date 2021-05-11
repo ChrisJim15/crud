f7b797b29d7cb8cc23fd6b48d8b46df0c85effc7
ChrisJim15 <83175358+ChrisJim15@users.noreply.github.com>
2021-05-11 22:29:21 +0800

    Add files via upload

---
+++ add.php
@@ -0,0 +1,22 @@
+<?php
+//including the database connection file
+include_once("config.php");
+
+//once the Submit button is clicked
+if(isset($_POST['Submit'])) {	
+	$name = mysqli_real_escape_string($mysqli, $_POST['name']);
+	$age = mysqli_real_escape_string($mysqli, $_POST['age']);
+	$email = mysqli_real_escape_string($mysqli, $_POST['email']);
+		
+	
+			
+		//insert data to database	
+		$result = mysqli_query($mysqli, "INSERT INTO users(name,age,email) VALUES('$name','$age','$email')");
+		
+		//display success message
+		echo "<font color='green'>Data added successfully.";
+		echo "<br/><a href='index.php'>View Result</a>";
+	
+}
+?>
+
---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$dbHost = 'localhost';
+$dbName = 'test';
+$dbUsername = 'root';
+$dbPassword = '';
+
+$mysqli = mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbName); 
+ 
+?>
